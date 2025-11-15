4cb66d687125519cbc9918a4eb346236f676e5ac
Victormarshall911 <favourjames1977@gmail.com>
2025-11-15 10:06:42 +0100

    Fixed comment errors

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -24,24 +24,32 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    """Serializer for user registration"""
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name', 'bio']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'bio': {'required': False}
+        }
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords don't match"})
+
+        if len(data['password']) < 6:
+            raise serializers.ValidationError({"password": "Password must be at least 6 characters"})
+
+        return data
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
-
 
 class UserUpdateSerializer(serializers.ModelSerializer):
     """Serializer for updating user profile"""
